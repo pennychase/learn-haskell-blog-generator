bc2b2c5ed2f686cd318767301630546f524c9a53
Penny Chase <mpchase@mac.com>
2021-11-02 23:33:06 -0400

    Refactored to use internal module (Section 3.7) and added additional HTML tags (Section3.8)

--- Html.hs
+++ Html.hs
@@ -6,59 +6,13 @@ module Html
     , html_
     , p_
     , h1_
+    , ul_
+    , ol_
+    , code_
     , append_
     , render
-    ) where
+    )
+    where
 
--- Types
-
-newtype Html = Html String
-
-newtype Structure = Structure String 
-
-type Title = String
-
--- Utilities
-escape :: String -> String
-escape =
-    let
-        escapeChar c =
-            case c of 
-                '<' -> "&lt;"
-                '>' -> "&gt;"
-                '&' -> "&amp;"
-                '"' -> "&quot;"
-                '\'' -> "&#39;"
-                _   -> [c]
-    in 
-        concat . map escapeChar
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure str1) (Structure str2) = Structure $ str1 <> str2
-
-getStructureString :: Structure -> String 
-getStructureString structure =
-    case structure of
-        Structure str -> str
-
-render :: Html -> String 
-render html =
-    case html of
-        Html str -> str
-
--- HTML generators
-
-el :: String -> String -> String
-el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-html_ :: Title -> Structure -> Html
-html_ title content = Html $ el "html"
-                           $ el "head" 
-                           $ el "title" (escape title) <> el "body" (getStructureString content)
-
-p_ :: String -> Structure
-p_ = Structure . el "p" . escape
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1" . escape
+import Html.Internal
 
---
+++ Html/Internal.hs
@@ -0,0 +1,62 @@
+module Html.Internal where
+
+-- Types
+
+newtype Html = Html String
+
+newtype Structure = Structure String 
+
+type Title = String
+
+-- Utilities
+escape :: String -> String
+escape =
+    let
+        escapeChar c =
+            case c of 
+                '<' -> "&lt;"
+                '>' -> "&gt;"
+                '&' -> "&amp;"
+                '"' -> "&quot;"
+                '\'' -> "&#39;"
+                _   -> [c]
+    in 
+        concat . map escapeChar
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure str1) (Structure str2) = Structure $ str1 <> str2
+
+getStructureString :: Structure -> String 
+getStructureString structure =
+    case structure of
+        Structure str -> str
+
+render :: Html -> String 
+render html =
+    case html of
+        Html str -> str
+
+-- HTML generators
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+html_ :: Title -> Structure -> Html
+html_ title content = Html $ el "html"
+                           $ el "head" 
+                           $ el "title" (escape title) <> el "body" (getStructureString content)
+
+p_ :: String -> Structure
+p_ = Structure . el "p" . escape
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" . escape
+
+ul_ :: [Structure] -> Structure
+ul_  = Structure . el "ul" . concatMap (el "li" . getStructureString) 
+
+ol_ :: [Structure] -> Structure
+ol_  = Structure . el "ol" . concatMap (el "li" . getStructureString) 
+
+code_ :: String -> Structure
+code_ = Structure . el "pre" . escape
