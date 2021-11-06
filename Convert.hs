54d9ce6b866ff39b10e58441a886ac74aec4e9d7
Penny Chase <mpchase@mac.com>
2021-11-06 12:58:04 -0400

    Initial 5.1 convert markup to html

---
+++ Convert.hs
@@ -0,0 +1,28 @@
+module Convert where
+
+import qualified Html
+import qualified Markup
+
+convertStructure :: Markup.Structure -> Html.Structure
+convertStructure structure =
+  case structure of
+    Markup.Header n txt ->
+      Html.h_ n txt
+
+    Markup.Paragraph p ->
+      Html.p_ p
+
+    Markup.UnorderedList list ->
+      Html.ul_ $ map Html.p_ list
+
+    Markup.OrderedList list ->
+      Html.ol_ $ map Html.p_ list
+
+    Markup.CodeBlock list ->
+      Html.code_ (unlines list)
+
+concatStructure :: [Html.Structure] -> Html.Structure
+concatStructure xs =
+  case xs of
+    [] -> Html.empty_
+    (y:ys) -> y <> concatStructure ys
