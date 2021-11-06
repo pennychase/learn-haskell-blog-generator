d1633c4ecb2ca302262735ca437fc7ba885e7409
Penny Chase <mpchase@mac.com>
2021-11-06 11:46:46 -0400

    moved parser into Markup.hs and deleted Parse.hs

--- Markup.hs
+++ Markup.hs
@@ -2,9 +2,11 @@ module Markup
     (
       Document
     , Structure (..)
+    , parse
     )
     where
 
+import Data.Maybe
 import Numeric.Natural
 
 type Document 
@@ -17,4 +19,60 @@ data Structure
     | OrderedList [String]
     | CodeBlock [String]
     deriving Show 
-    
+
+parse :: String -> Document
+parse = parseLines Nothing . lines
+
+parseLines :: Maybe Structure -> [String] -> Document
+parseLines context txts =
+  case txts of
+    -- done case
+    [] -> maybeToList context
+   
+    -- Header 1 case
+    ('*' : ' ' : line) : rest ->
+      maybe id (:) context (Header 1 (trim line) : parseLines Nothing rest)
+   
+    -- Unordered list case
+    ('-' : ' ' : line) : rest ->
+      case context of
+        Just (UnorderedList list) ->
+          parseLines (Just (UnorderedList (list <> [trim line]))) rest
+
+        _ ->
+          maybe id (:) context (parseLines (Just (UnorderedList [trim line])) rest)
+
+    -- Ordered list case
+    ('#' : ' ' : line) : rest ->
+      case context of
+        Just (OrderedList list) -> 
+          parseLines (Just (OrderedList (list <> [trim line]))) rest
+        _ ->
+          maybe id (:) context (parseLines (Just (OrderedList [trim line])) rest)
+
+    -- Code Block case
+    ('>' : ' ' : line) : rest ->
+      case context of
+        Just (CodeBlock code) ->
+          parseLines (Just (CodeBlock (code <> [trim line]))) rest
+        _ ->
+          maybe id (:) context (parseLines (Just (CodeBlock [trim line])) rest)
+
+
+    -- Paragraph case
+    currentLine : rest ->
+      let
+        line = trim currentLine
+      in
+        if line == ""
+          then
+            maybe id (:) context (parseLines Nothing rest)
+          else
+            case context of
+              Just (Paragraph paragraph) ->
+                parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
+              _ ->
+                maybe id (:) context (parseLines (Just (Paragraph line)) rest)
+
+trim :: String -> String
+trim = unwords . words
